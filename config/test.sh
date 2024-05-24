680e961e7c58469dba75a85feab6b84aaf6664f8
Johannes Gaiser <johannes.gaiser@gamlo.de>
2024-05-24 16:21:38 +0200

    testbed config

--- config/test.sh
+++ config/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ip netns exec net2 snabb hybrid_access "/home/student/snabb/config/config-receiver.ini" &
-snabb hybrid_access "/home/student/snabb/config/config-sender.ini"
+ip netns exec net2 snabb hybrid_access "/home/student/snabb/config/config-receiver-t.ini" &
+snabb hybrid_access "/home/student/snabb/config/config-sender-t.ini"
 
 wait 
 echo "All done"
