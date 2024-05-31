b714902ed3c1b8f2a44715db308b12c729d0ee0a
GrafZ3pp3lin <grafzeppelin@gamlo.de>
2024-05-31 13:09:37 +0200

    fix pci

--- src/program/example_source/example_source.lua
+++ src/program/example_source/example_source.lua
@@ -8,8 +8,8 @@ local intel_nic = require("apps.intel_avf.intel_avf")
 function run(args)
     local c = config.new()
     config.app(c, "source", basic.Source)
-    config.app(c, "nic", intel_nic.Intel_avf, { pciaddr = "0000:00:10:0", nqueues = 1 })
-    config.app(c, "link", intel_nic.IO, { pciaddr = "0000:00:10:0", queue = 0 })
+    config.app(c, "nic", intel_nic.Intel_avf, { pciaddr = "0000:00:10.0", nqueues = 1 })
+    config.app(c, "link", intel_nic.IO, { pciaddr = "0000:00:10.0", queue = 0 })
 
     config.link(c, "source.output -> link.input")
 
--- src/program/example_target/example_target.lua
+++ src/program/example_target/example_target.lua
@@ -7,8 +7,8 @@ local intel_nic = require("apps.intel_avf.intel_avf")
 
 function run(args)
     local c = config.new()
-    config.app(c, "nic", intel_nic.Intel_avf, { pciaddr = "0000:00:1b:0", nqueues = 1 })
-    config.app(c, "link", intel_nic.IO, { pciaddr = "0000:00:1b:0", queue = 0 })
+    config.app(c, "nic", intel_nic.Intel_avf, { pciaddr = "0000:00:1b.0", nqueues = 1 })
+    config.app(c, "link", intel_nic.IO, { pciaddr = "0000:00:1b.0", queue = 0 })
     config.app(c, "sink", basic.Sink)
 
     config.link(c, "link.output -> sink.input")
