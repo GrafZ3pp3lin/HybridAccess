8ca8b650d84d9f75b966b769f2f35873a337ef59
GrafZ3pp3lin <grafzeppelin@gamlo.de>
2024-05-31 15:44:36 +0200

    pcap example

--- src/program/example_source/example_source.lua
+++ src/program/example_source/example_source.lua
@@ -2,6 +2,7 @@
 
 module(..., package.seeall)
 
+local pcap = require("apps.pcap.pcap")
 local synth = require("apps.test.synth")
 local intel_nic = require("apps.intel_avf.intel_avf")
 
@@ -16,12 +17,12 @@ function run()
        "Driver should be apps.intel_avf.intel_avf (is "..info.driver..")")
 
     local c = config.new()
-    config.app(c, "synth", synth.Synth)
+    config.app(c, "source", pcap.PcapReader, "config/input.pcap")
     config.app(c, "nic", intel_nic.Intel_avf, { pciaddr = pciaddr, nqueues = 1 })
     config.app(c, "link", intel_nic.IO, { pciaddr = pciaddr, queue = 0 })
 
-    config.link(c, "synth.output -> link.input")
+    config.link(c, "source.output -> link.input")
 
     engine.configure(c)
-    engine.main({ duration = 10, report = { showlinks = true, showapps = true } })
+    engine.main({ duration = 1, report = { showlinks = true, showapps = true } })
 end
