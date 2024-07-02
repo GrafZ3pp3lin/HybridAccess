7de09f5d449ff1c92a2521427a1895007ad88caf
GrafZ3pp3lin <grafzeppelin@gamlo.de>
2024-07-02 14:49:04 +0200

    update forwarder

--- src/program/hybrid_access/middleware/mac_forwarder.lua
+++ src/program/hybrid_access/middleware/mac_forwarder.lua
@@ -4,6 +4,9 @@ local ffi = require("ffi")
 local link = require("core.link")
 local ethernet = require("lib.protocol.ethernet")
 
+local copy = ffi.copy
+local receive, transmit, nreadable = link.receive, link.transmit, link.nreadable
+
 MacForwarder = {}
 MacForwarder.config = {
     source_mac = { required = true },
@@ -11,9 +14,14 @@ MacForwarder.config = {
 }
 
 function MacForwarder:new(conf)
+    local source_mac = ethernet:pton(conf.source_mac)
+    local destination_mac = ethernet:pton(conf.destination_mac)
+    local eth_header_addr = ffi.new("uint8_t[12]")
+    copy(eth_header_addr, destination_mac, 6)
+    copy(eth_header_addr + 6, source_mac, 6)
+
     local o = {
-        source_mac = ethernet:pton(conf.source_mac),
-        destination_mac = ethernet:pton(conf.destination_mac)
+        header_addr = eth_header_addr
     }
     setmetatable(o, self)
     self.__index = self
@@ -24,10 +32,10 @@ function MacForwarder:push()
     local input = assert(self.input.input, "input port not found")
     local output = assert(self.output.output, "output port not found")
 
-    for _ = 1, link.nreadable(input) do
-        local p = link.receive(input)
-        ffi.copy(p.data, self.destination_mac, 6)
-        ffi.copy(p.data + 6, self.source_mac, 6)
-        link.transmit(output, p)
+    local incoming = nreadable(input)
+    for _ = 1, incoming do
+        local p = receive(input)
+        copy(p.data, self.header_addr, 12)
+        transmit(output, p)
     end
 end
