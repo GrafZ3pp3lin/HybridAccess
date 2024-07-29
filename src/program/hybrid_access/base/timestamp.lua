7c7028e18d6d0e99f7ecfebc89592881ada2a8d1
GrafZ3pp3lin <grafzeppelin@gamlo.de>
2024-07-29 12:25:58 +0200

    use timestamp struct

--- src/program/hybrid_access/base/timestamp.lua
+++ src/program/hybrid_access/base/timestamp.lua
@@ -1,23 +1,36 @@
+---@diagnostic disable: inject-field, param-type-mismatch, undefined-field
 local ffi = require("ffi")
 
 local cast, copy = ffi.cast, ffi.copy
 
-local longp = ffi.typeof("uint64_t*")
+local pkt_timestamp = ffi.typeof([[
+   struct {
+      uint8_t  marker;
+      uint64_t timestamp;
+   } __attribute__((packed))
+]])
+local pkt_timestamp_p = ffi.typeof("$*", pkt_timestamp)
+local pkt_timestamp_len = ffi.sizeof(pkt_timestamp)
+
 local timestamp_marker = 0x55
 
 function timestamp_to_pointer(timestamp)
-    local ctp = cast(longp, timestamp)
-    return ctp
+    local obj = ffi.new(pkt_timestamp)
+    obj.marker = timestamp_marker
+    obj.timestamp = timestamp
+    local obj_p = cast(pkt_timestamp_p, obj)
+    return obj_p
 end
 
 function append_timestamp(pkt, tsp)
-    pkt.data[pkt.length] = timestamp_marker
-    copy(pkt.data + pkt.length + 1, tsp, 8)
+    copy(pkt.data + pkt.length, tsp, pkt_timestamp_len)
 end
 
 function get_timestamp(pkt)
-    if pkt.data[pkt.length] ~= timestamp_marker then
+    local obj_p = cast(pkt_timestamp_p, pkt.data + pkt.length)
+    if obj_p.marker ~= timestamp_marker then
         return nil
+    else
+        return obj_p.timestamp
     end
-    return cast(longp, pkt.data + pkt.length + 1)
 end
