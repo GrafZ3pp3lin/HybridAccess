857d15253f79b5847424500fd5508bcf44db3b8e
GrafZ3pp3lin <grafzeppelin@gamlo.de>
2024-07-29 12:16:22 +0200

    test

--- src/program/example_test/example_test.lua
+++ src/program/example_test/example_test.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-field, inject-field
 module(..., package.seeall)
 
 local ts = require("program.hybrid_access.base.timestamp")
@@ -5,17 +6,28 @@ local ts = require("program.hybrid_access.base.timestamp")
 local ffi = require('ffi')
 local C = ffi.C
 
+local test_struct = ffi.typeof([[
+   struct {
+      uint8_t  marker;
+      uint64_t timestamp;
+   } __attribute__((packed))
+]])
+local test_struct_p = ffi.typeof("$*", test_struct)
+
 function run ()
     local time_now = C.get_time_ns()
     print(time_now)
-    local long = ffi.new("uint64_t", 0)
-    print(long)
-    local lp = ffi.cast("uint64_t*", long)
-    local ctp = ffi.cast("uint64_t*", time_now)
+    local strct = ffi.new(test_struct)
+    strct.marker = 0x55
+    strct.timestamp = time_now
+    local lp = ffi.cast(test_struct_p, strct)
     -- local time_now_p = ts.timestamp_to_pointer(time_now)
-    print(ctp)
+    print(lp.timestamp)
     -- print(ctp[0])
     -- print(time_now == ctp[0])
-    ffi.copy(lp, ctp, 8)
-    print(long)
+    local strct2 = ffi.new(test_struct)
+    local lp2 = ffi.cast(test_struct_p, strct2)
+    print(lp2.timestamp)
+    ffi.copy(lp2, lp, 9)
+    print(lp2.timestamp)
 end
