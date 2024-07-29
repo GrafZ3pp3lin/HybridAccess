fb649273825466619b3346d3b5f27d1b4c131964
GrafZ3pp3lin <grafzeppelin@gamlo.de>
2024-07-29 12:04:04 +0200

    test

--- src/program/example_test/example_test.lua
+++ src/program/example_test/example_test.lua
@@ -8,7 +8,7 @@ local C = ffi.C
 function run ()
     local time_now = C.get_time_ns()
     print(time_now)
-    local ctp = ffi.cast("uint64_t*", time_now)
+    local ctp = tonumber(ffi.cast("uint64_t*", time_now))
     -- local time_now_p = ts.timestamp_to_pointer(time_now)
     print(ctp)
     print(ctp[0])
